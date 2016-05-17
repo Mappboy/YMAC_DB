83e164bf1d1096979d6d93e77e572738e219473b
Pooli3 <Cameron.j.poole@gmail.com>
2016-05-17 10:43:13 +0800

    Set admin as index

--- ymac_sdb/urls.py
+++ ymac_sdb/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib.gis import admin
-
 from ymac_db import views
 
 urlpatterns = [
-    url(r'^$', admin.site.urls),
-    # url(r'^$', views.index, name='index'),
+    url(r'^home/', views.index, name='index'),
+    url(r'^', admin.site.urls),
 ]
