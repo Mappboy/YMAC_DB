c6937ed7316863126f3f7a32a532947d5e892d83
Cameron Poole <Cameron.j.poole@gmail.com>
2016-11-29 16:49:25 +0800

    Fix for survey update

--- ymac_db/admin.py
+++ ymac_db/admin.py
@@ -1380,12 +1380,45 @@ def export_as_json(modeladmin, request, queryset):
 
 
 def export_as_csv(modeladmin, request, queryset):
-    import csv
+    import sys
+    import csv, codecs
+    class UnicodeWriter:
+        """
+        A CSV writer which will write rows to CSV file "f",
+        which is encoded in the given encoding.
+        """
+
+        def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+            from cStringIO import StringIO
+            # Redirect output to a queue
+            self.queue = StringIO()
+            self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+            self.stream = f
+            self.encoder = codecs.getincrementalencoder(encoding)()
+
+        def writerow(self, row):
+            self.writer.writerow([s.encode("utf-8") for s in row])
+            # Fetch UTF-8 output from the queue ...
+            data = self.queue.getvalue()
+            data = data.decode("utf-8")
+            # ... and reencode it into the target encoding
+            data = self.encoder.encode(data)
+            # write to the target stream
+            self.stream.write(data)
+            # empty queue
+            self.queue.truncate(0)
+
+        def writerows(self, rows):
+            for row in rows:
+                self.writerow(row)
+
     # Create the HttpResponse object with the appropriate CSV header.
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="heritage_surveys.csv"'
-
-    writer = csv.writer(response)
+    if sys.version_info[0] == 2:
+        writer = UnicodeWriter(response)
+    else:
+        writer = csv.writer(response)
     fields = (
         'survey_id',
         'date_from',
